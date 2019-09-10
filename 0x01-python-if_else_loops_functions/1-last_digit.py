f867927608e78c01ffe91fae18f08d62767dae3b
Diego Monroy <difemo91@gmail.com>
2019-09-10 23:29:38 +0000

    add last digit python script

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-def uppercase(str):
+import random
+
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    non_negative = number * -1
+else:
+    non_negative = number
+
+last = non_negative % 10
+
+if last > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last))
+elif last < 6 and last != 0:
+    print("Last digit of {:d} is {:d} and is less that 6 and not 0".format(number, last))
+else:
+    print("Last digit of {:d} is {:d} and is zero".format(number, last))
