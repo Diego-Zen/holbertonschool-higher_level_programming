f8f7bbeb25a1a6a7f8eab3b8b861ce2357901d01
Diego Monroy <difemo91@gmail.com>
2019-09-10 22:48:12 +0000

    delete semicolon

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
 
 if number > 0:
@@ -7,4 +8,4 @@ if number > 0:
 elif number < 0:
     print("{:d} is negative".format(number))
 else:
-    print("{:d} is zero".format(number));
+    print("{:d} is zero".format(number))
