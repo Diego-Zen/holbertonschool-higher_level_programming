f598bad191cc2c152f92550ccdef61606368319b
Diego Monroy <difemo91@gmail.com>
2019-09-11 05:00:49 +0000

    add magic calculation

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0:
-            if i % 5 == 0:
-                print("{}".format("FizzBuzz"), end=" ")
-            else:
-                print("{}".format("Fizz"), end=" ")
-        elif i % 5 == 0:
-            print("{}".format("Buzz"), end=" ")
-        else:
-            print("{:d}".format(i), end=" ")
+def magic_calculation(a, b, c):
+    return len(a)
