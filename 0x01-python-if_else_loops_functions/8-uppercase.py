f4dd9b655940b62c63d735c9872c8df0481d0c10
Diego Monroy <difemo91@gmail.com>
2019-09-11 04:23:51 +0000

    add uppercase python script

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        asc = ord(str[i])
+        if asc < 123 and asc > 96:
+            asc -= 32
+        print("{:c}".format(asc), end="")
+    print()
