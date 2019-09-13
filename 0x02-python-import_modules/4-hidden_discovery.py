2ad0cf5fed03ff4b1e6e5d4018ff849bb21f8f3d
Diego Monroy <difemo91@gmail.com>
2019-09-13 01:59:36 +0000

    add hidden names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    import sys
+
+    names = dir(hidden_4)
+
+    for i in range(len(names)):
+        if names[i][:1] != "_":
+            print("{}".format(names[i]))
