6f42beb95ef6679abf54ae25b806479f3be21c18
Diego Monroy <difemo91@gmail.com>
2019-09-13 00:57:04 +0000

    add infinite addition python script

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    arguments = sys.argv
+    length = len(sys.argv)
+    result = 0
+
+    if length == 1:
+        print("{:d}".format(length - 1))
+    else:
+        for i in range(1, length):
+            result += int(arguments[i])
+        print("{:d}".format(result))
