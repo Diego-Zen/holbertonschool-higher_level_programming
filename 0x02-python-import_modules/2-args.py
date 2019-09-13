6623698627d305ad4c4353fdb14a98f2d04ae29b
Diego Monroy <difemo91@gmail.com>
2019-09-13 00:46:31 +0000

    add arguments python script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    arguments = sys.argv
+    length = len(sys.argv)
+    argslen = length - 1
+
+    if length == 1:
+        print("{:d} {}".format(argslen, "arguments."))
+    elif length == 2:
+        print("{:d} {}".format(argslen, "argument:"))
+        print("{:d}: {}".format(argslen, arguments[1]))
+    else:
+        print("{:d} {}".format(argslen, "arguments:"))
+        for i in range(1, length):
+            print("{:d}: {}".format(i, arguments[i]))
