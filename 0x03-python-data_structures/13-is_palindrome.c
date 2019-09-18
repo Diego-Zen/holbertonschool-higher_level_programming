f7527e142a385ef5753f9cd3bce0a2e10e8b0ba7
Diego Monroy <difemo91@gmail.com>
2019-09-18 04:51:54 +0000

    add initial files is palindrome c program

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ *
+ * @head: pointer head list
+ *
+ * Return: 0 if is not a palindrome or 1 if it is
+ */
+int is_palindrome(listint_t **head)
+{
+	return (1);
+}
