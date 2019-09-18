e2ff08824b0b7da36d9029168fd64df9202efb03
Diego Monroy <difemo91@gmail.com>
2019-09-18 03:49:26 +0000

    add max integer of a list python script

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if my_list is not None:
+        if len(my_list) > 0:
+            my_list.sort()
+            print(my_list)
+            return my_list[-1]
+        else:
+            return None
