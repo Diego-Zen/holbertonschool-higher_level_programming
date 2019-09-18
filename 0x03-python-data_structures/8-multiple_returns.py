b8c0602712a32e5b5e127006fd7d7f77dcb20d56
Diego Monroy <difemo91@gmail.com>
2019-09-18 03:31:56 +0000

    add multiple returns python script

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if sentence is None:
+        my_tuple = 0, None
+        return my_tuple
+
+    length = 0
+    for i in sentence:
+        length += 1
+
+    my_tuple = length, sentence[0]
+    return my_tuple
