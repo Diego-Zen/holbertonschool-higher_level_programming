0669da104dbaef0bc45d7a6af0d43ca8067bb9c6
Diego Monroy <difemo91@gmail.com>
2019-09-18 02:39:04 +0000

    add print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is not None:
+        for i in matrix:
+            length = len(i)
+            x = 1
+            for j in i:
+                if x == length:
+                    print("{:d}".format(j), end="")
+                else:
+                    print("{:d}".format(j), end=" ")
+                x += 1
+            print()
