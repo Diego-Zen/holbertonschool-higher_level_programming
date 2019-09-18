85d2b60ff1b88ba32bbc1c01e95b337ea012c87a
Diego Monroy <difemo91@gmail.com>
2019-09-18 02:14:22 +0000

    add remove characters python script

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    my_list = []
+    for i in my_string:
+        if i == "C" or i == "c":
+            continue
+        my_list.append(i)
+    new_string = ''.join(my_list)
+    return new_string
