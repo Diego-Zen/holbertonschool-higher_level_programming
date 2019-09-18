b82b9431e9cd178098512731a27885c76be8b4ba
Diego Monroy <difemo91@gmail.com>
2019-09-18 01:57:50 +0000

    add new in list python script

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    length = len(my_list) - 1
+    my_new_list = []
+
+    for i in my_list:
+        my_new_list.append(i)
+
+    if idx < 0 or idx > length:
+        return my_new_list
+
+    my_new_list[idx] = element
+
+    return my_new_list
