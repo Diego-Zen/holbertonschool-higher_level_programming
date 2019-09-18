151bb9e9835a82314363e9464884f67b5148b42f
Diego Monroy <difemo91@gmail.com>
2019-09-18 03:39:03 +0000

    add len function

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        my_tuple = 0, None
+        my_tuple = len(sentence), None
         return my_tuple
 
     my_tuple = len(sentence), sentence[0]
