09d679a7db9032c16854470029d9ef58afdb3781
Diego Monroy <difemo91@gmail.com>
2019-09-18 03:25:42 +0000

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    length_tuple_a = len(tuple_a)
+    length_tuple_b = len(tuple_b)
+    my_list_a = []
+    my_list_b = []
+
+    for i in range(length_tuple_a):
+        my_list_a.append(tuple_a[i])
+    for i in range(length_tuple_b):
+        my_list_b.append(tuple_b[i])
+
+    # print(my_list_a)
+    # print(my_list_b)
+
+    if length_tuple_a < 2:
+        for i in range(length_tuple_a, 2):
+            my_list_a.append(0)
+
+    # print(my_list_a)
+
+    if length_tuple_b < 2:
+        for i in range(length_tuple_b, 2):
+            my_list_b.append(0)
+
+    # print(my_list_b)
+
+    new_tuple = my_list_a[0] + my_list_b[0], my_list_a[1] + my_list_b[1]
+
+    return new_tuple
