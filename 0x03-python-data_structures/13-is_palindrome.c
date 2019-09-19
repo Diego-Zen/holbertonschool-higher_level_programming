606723879b28973ec24ee070ebc69e9d24e9e27a
Diego Monroy <difemo91@gmail.com>
2019-09-19 03:05:34 +0000

    add palindrome C program

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,8 +8,37 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *tmp1 = *head;
+	listint_t *tmp2 = *head;
+
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	int length = 0;
+
 	if (*head == NULL)
 		return (1);
 
-	return (0);
+	while (tmp2->next != NULL)
+	{
+		tmp2 = tmp2->next;
+		i++;
+	}
+	length = (i + 1) / 2;
+	while (j < length)
+	{
+		x = 0;
+		if (tmp1->n != tmp2->n)
+			return (0);
+		tmp2 = *head;
+		tmp1 = tmp1->next;
+		i--;
+		while (x < i)
+		{
+			tmp2 = tmp2->next;
+			x++;
+		}
+		j++;
+	}
+	return (1);
 }
