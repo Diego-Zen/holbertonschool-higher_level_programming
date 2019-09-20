1f93c6c3fcd4ef9c7c759793ca4d6e2ebb9198ee
Diego Monroy <difemo91@gmail.com>
2019-09-20 04:14:33 +0000

    add update dictionary python script

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
+def update_dictionary(a_dictionary: dict, key: str, value: any):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        new = {key: value}
+        a_dictionary.update(new)
+    return a_dictionary
