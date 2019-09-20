658a3aac7b8569b6aced2bc21b06722741460540
Diego Monroy <difemo91@gmail.com>
2019-09-20 04:35:42 +0000

    add multiply by 2 python script

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
+def multiply_by_2(a_dictionary: dict):
+    new_dict = a_dictionary.copy()
+    for k, v in a_dictionary.items():
+        new_dict[k] = v * 2
+    return new_dict
