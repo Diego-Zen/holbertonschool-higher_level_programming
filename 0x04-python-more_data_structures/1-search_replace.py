a40e01a0ed456271da09f2f02d1a7c3553288730
Diego Monroy <difemo91@gmail.com>
2019-09-20 02:18:26 +0000

    add search and replace python script

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
+def search_replace(my_list: list, search: int, replace: int):
+    new_list = [replace if i == search else i for i in my_list]
+    return new_list
