19eb33a9d690c427359b63bed602c89486d50fe7
Diego Monroy <difemo91@gmail.com>
2019-09-20 03:49:41 +0000

    add print sorted dictionary python script

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
+def print_sorted_dictionary(a_dictionary: dict):
+    new_dict = sorted(a_dictionary.items())
+    for tupl in new_dict:
+        print("{}: {}".format(tupl[0], tupl[1]))
