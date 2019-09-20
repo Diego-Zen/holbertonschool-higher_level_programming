fdb22478f0ab84b6f9e33e523cca0b56aeac9915
Diego Monroy <difemo91@gmail.com>
2019-09-20 01:47:10 +0000

    add return to function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_clone = []
+    matrix_clone = list()
     for i in matrix:
         matrix_clone.append(list(map((lambda x: x ** 2), i)))
-    print(matrix_clone)
+    return matrix_clone
