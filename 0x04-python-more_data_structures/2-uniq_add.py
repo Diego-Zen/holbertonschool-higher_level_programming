b7436b0546ce942423e18d54ddac58fb1162399d
Diego Monroy <difemo91@gmail.com>
2019-09-20 03:05:28 +0000

    add uniq add python script

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    sum = 0
+    my_list.sort()
+
+    for idx, i in enumerate(my_list):
+        if (idx > 0):
+            if i == my_list[idx - 1]:
+                continue
+        sum += i
+    return sum
