cc6645eb0eb4f4fb3161f27c983ed93c3651cb12
Diego Monroy <difemo91@gmail.com>
2019-10-01 02:19:05 +0000

    add safe print list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    elements = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            elements += 1
+
+        except:
+            pass
+
+    print()
+    return elements
