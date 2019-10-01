3c77af91e8377b0731ed08f1b21d2a7de95f5464
Diego Monroy <difemo91@gmail.com>
2019-10-01 04:44:39 +0000

    add safe print integer error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+import sys
+
+
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
 
     except:
+        sys.stderr.write("Exception: ")
         return False
