d158b3c4e891ac3e338601b90f2a44aeabad8fba
Diego Monroy <difemo91@gmail.com>
2019-10-02 03:37:25 +0000

    add square size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+
+class Square:
+    """
+    Defines a square
+    Args:
+        size (int): size of a square
+    """
+    def __init__(self, size=0):
+        if type(size) == int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
