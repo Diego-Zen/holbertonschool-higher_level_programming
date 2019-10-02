2d9e995726cf7759a632f0f75a16d1cee79cffff
Diego Monroy <difemo91@gmail.com>
2019-10-02 03:43:26 +0000

    add area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+
+class Square:
+    """
+    Defines a square
+    Args:
+        size (int): size of a square
+    """
+    def __init__(self, size=0):
+        if type(size) == int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    """
+    Area of a square
+    """
+    def area(self):
+        return self.__size ** 2
