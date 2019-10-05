67eb5b91ec3beebe1a1f10ce14b8f496056e513f
Diego Monroy <difemo91@gmail.com>
2019-10-05 01:57:13 +0000

    add squeare size

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+
+
+class Square:
+    """
+    Defines a square
+    Args:
+        size (int): size of a square
+
+    Raises:
+        TypeError: if size is not int
+        ValueError: if size is less than 0
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        size (int): get size property and set size
+
+        Raises:
+            TypeError: if size is not int
+            ValueError: if size is less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) == int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    """
+    Area of a square
+
+    Returns:
+        the area of a square (size ** 2)
+    """
+    def area(self):
+        return self.__size ** 2
