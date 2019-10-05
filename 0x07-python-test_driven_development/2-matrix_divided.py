999f6153c9a21a8794a6d85fd38e7e396c4f314e
Diego Monroy <difemo91@gmail.com>
2019-10-05 02:55:10 +0000

    add matrix divided

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Divide a matrix module
+"""
+
+
+def matrix_divided(matrix, div):
+    """ divides all elements of a matrix """
+    matrix_clone = list()
+    if matrix is None:
+        raise TypeError("Matrix must be a matrix of integers/floats")
+    if div is 0:
+        raise ZeroDivisionError("division by zero")
+    if len(matrix[0]) != len(matrix[1]):
+        raise TypeError("Each row of the matrix must have the same size")
+    for i in matrix:
+        matrix_clone.append(list(map((lambda x: test_case(x, div)), i)))
+    return matrix_clone
+
+
+def test_case(x, div):
+    """ test type error """
+    if type(x) is not int and type(x) is not float:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/\
+        floats")
+    else:
+        result = round(x / div, 2)
+        return result
