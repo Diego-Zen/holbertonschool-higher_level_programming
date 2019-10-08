568c6a45bd3b38a912376e17a480770d6b2126d8
Diego Monroy <difemo91@gmail.com>
2019-10-08 05:17:36 +0000

    add initial code

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+"""
+   Real definition of a rectangle module
+"""
+
+
+class Rectangle:
+    """ defines a rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ innitialize the data """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ retrieve width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ set value if is int and > 0, or raise error """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ retrieve height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ set height if is int and > 0 or raise error """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
