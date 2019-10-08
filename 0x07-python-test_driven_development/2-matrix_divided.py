87288ddbcc1f055e47445b44f5fabd6ea6ab1c6b
Diego Monroy <difemo91@gmail.com>
2019-10-08 05:24:46 +0000

    add new test matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,8 +11,9 @@ def matrix_divided(matrix, div):
         raise TypeError("Matrix must be a matrix of integers/floats")
     if div is 0:
         raise ZeroDivisionError("division by zero")
-    if len(matrix[0]) != len(matrix[1]):
-        raise TypeError("Each row of the matrix must have the same size")
+    if len(matrix) > 1:
+        if len(matrix[0]) != len(matrix[1]):
+            raise TypeError("Each row of the matrix must have the same size")
     for i in matrix:
         matrix_clone.append(list(map((lambda x: test_case(x, div)), i)))
     return matrix_clone
