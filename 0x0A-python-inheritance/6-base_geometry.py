acf5b9ab22314c9ec980bf24acc4fed88f99b067
Diego Monroy <difemo91@gmail.com>
2019-10-16 04:50:12 +0000

    add base geometry exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+    improve geometry module
+"""
+
+
+class BaseGeometry():
+    """ class base on base_geometry """
+
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
