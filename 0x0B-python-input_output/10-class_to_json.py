4c77e8e012c5b52f133daee77fa1e66813a7ceed
Diego Monroy <difemo91@gmail.com>
2019-10-17 04:09:29 +0000

    add class to json python script

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+    Class to JSON module
+"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description fpr JSON serialization of an object
+
+    Args:
+        obj (obj): instance of a class
+
+    Returns:
+        dict: dictionary description
+    """
+    return obj.__dict__
