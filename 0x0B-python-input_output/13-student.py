83658f26f7718050ab8b6da27238c8ea9fa19750
Diego Monroy <difemo91@gmail.com>
2019-10-17 04:56:37 +0000

    add student to disk and reload

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+"""
+    Student to disk and reload
+"""
+
+
+class Student():
+    """ defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name (str): first name student
+            last_name (str): last name student
+            age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a student instance
+        Args:
+            attrs (list): atributes
+
+        Returns:
+            dict: dictionary representation
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            my_dict = self.__dict__
+            new_dict = {}
+            for attr in attrs:
+                if attr in my_dict:
+                    new = {attr: self.__dict__[attr]}
+                    new_dict.update(new)
+            return new_dict
+
+    def reload_from_json(self, json):
+        """replace all attributes of the Student instance
+
+        Args:
+            json (json): dictionary
+        """
+        for item in json.items():
+            setattr(self, item[0], item[1])
