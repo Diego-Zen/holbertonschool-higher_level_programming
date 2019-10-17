618ff300d36f0e1e179028200aa2a8650eadb747
Diego Monroy <difemo91@gmail.com>
2019-10-17 01:34:36 +0000

    add write to a file python script

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""
+    Write to a file module
+"""
+
+
 def write_file(filename="", text=""):
+    """writes a string to a text file
+    Args:
+        filename (str): file name
+        text (str): text to write
+
+    Returns:
+        int: number of characters written
+    """
+    with open(filename, mode='w', encoding='utf-8') as fn:
+        return (fn.write(text))
