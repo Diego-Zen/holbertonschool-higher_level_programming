8b39d055a5255fda0d702d062422bac7a27fad67
Diego Monroy <difemo91@gmail.com>
2019-10-17 01:44:22 +0000

    add append to a file python script

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+"""
+    Append to a file module
+"""
+
+
 def append_write(filename="", text=""):
+    """appends a string at the end of a text file
+
+    Args:
+        filename (str): file name
+        text (str): text to append
+
+    Returns:
+        int: number of characters added
+    """
+    with open(filename, mode='a', encoding='utf-8') as fn:
+        return (fn.write(text))
