b417cfe5d23b25e7fb15b7224ca9c3b7423f3652
Diego Monroy <difemo91@gmail.com>
2019-10-17 05:02:07 +0000

    add pascal triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    Pascal's triangle module
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal's triangle
+
+    Args:
+        n (int): numbers of lists
+
+    Returns:
+        list: list of lists
+    """
+    my_list = []
+    if n <= 0:
+        return my_list
