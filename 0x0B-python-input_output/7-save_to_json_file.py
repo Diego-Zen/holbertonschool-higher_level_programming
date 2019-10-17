64048fa18c39b916e0c09c42d39fcba2d92b2cc2
Diego Monroy <difemo91@gmail.com>
2019-10-17 02:27:08 +0000

    add save to json file python script

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+import json
+"""
+    Save object to a file module
+"""
+
+
 def save_to_json_file(my_obj, filename):
+    """writes an object to a text file using a JSON representation
+
+    Args:
+        my_obj (obj): object
+        filename (str): file name
+    """
+    with open(filename, mode='w', encoding='utf-8') as fn:
+        fn.write(json.dumps(my_obj))
