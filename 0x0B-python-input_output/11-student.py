c7531a86389119cc3f157a94e0f8291ce99214a8
Diego Monroy <difemo91@gmail.com>
2019-10-17 04:19:26 +0000

    add student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+    Student to JSON
+"""
+
+
+class Student():
+    """ defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name (str): first name student
+            last_name (str): last name student
+            age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a student instance """
+        return self.__dict__
