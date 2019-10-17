4191a1026f4e95519d720f7649b2eb8ef2636d5f
Diego Monroy <difemo91@gmail.com>
2019-10-17 00:17:06 +0000

    add number of lines python script

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""
+    NUmber of lines module
+"""
+
+
 def number_of_lines(filename=""):
+    """number of lines of a text file
+
+    Args:
+        filename (string): file name or relative path
+
+    Returns:
+        int: number of lines
+    """
+    n_lines = 0
+
+    with open(filename, encoding="utf-8") as fn:
+        for line in fn:
+            n_lines += 1
+
+    return n_lines
