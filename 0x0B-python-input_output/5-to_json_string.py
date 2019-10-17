0cfa8404aa400af1e5a6c1162f4006d9aa949db4
Diego Monroy <difemo91@gmail.com>
2019-10-17 01:53:01 +0000

    add json serialized

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+import json
+"""
+    To JSON string module
+"""
+
+
 def to_json_string(my_obj):
+    """returns the JSON representation of an object
+
+    Args:
+        my_obj (str): object
+
+    Returns:
+        json: representation of an object
+    """
+    return (json.dumps(my_obj))
