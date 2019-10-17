88cbf1a165be2352a82ec8bb8457e15cc1422c8e
Diego Monroy <difemo91@gmail.com>
2019-10-17 00:03:29 +0000

    initial files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Read file module
+"""
+
+
+def read_file(filename=""):
+    """reads a text file and prints it to stdout
+
+    Args:
+        filename (string): file name
+    """
+    with open(filename, encoding="utf-8") as fn:
+        for line in fn:
+            print(line)
