d837cd8aa06ecb8f5adf1d7362b091d1e382e43a
Diego Monroy <difemo91@gmail.com>
2019-10-17 02:06:58 +0000

    add json to object

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+import json
+from io import StringIO
+"""
+    From JSON string to object module
+"""
+
+
 def from_json_string(my_str):
+    """returns an object represented by a JSON
+
+    Args:
+        my_str (str): JSON representation
+
+    Returns:
+        obj: object Python data structure
+    """
+    io = StringIO(my_str)
+    return (json.load(io))
