28202e7351dba3db883a117fb7c4f718e959b215
Diego Monroy <difemo91@gmail.com>
2019-10-17 03:39:01 +0000

    add item list python script

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""
+    Load, add, save module
+"""
+if __name__ == "__main__":
+
+    import sys
+    import json
+
+    load_json = __import__('8-load_from_json_file').load_from_json_file
+    save_json = __import__('7-save_to_json_file').save_to_json_file
+
+    filename = "add_item.json"
+
+    try:
+        my_list = load_json(filename)
+    except:
+        my_list = []
+
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+
+    save_json(my_list, filename)
