54d9be24f084cc0bd7e5c2989ee47110c1fce7c1
Diego Monroy <difemo91@gmail.com>
2019-10-17 03:06:53 +0000

    add load form json file python script

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+import json
+from io import StringIO
+"""
+    Create object from a JSON file module
+"""
+
+
 def load_from_json_file(filename):
+    """creates an object from a JSON file
+
+    Args:
+        filename (str): file name
+
+    Returns:
+        obj: object
+    """
+    with open(filename, encoding='utf-8') as fn:
+        r_file = fn.read()
+        io = StringIO(r_file)
+        return (json.load(io))
