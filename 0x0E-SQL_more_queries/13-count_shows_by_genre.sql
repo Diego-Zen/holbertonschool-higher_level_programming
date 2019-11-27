fa0431dc54acd731a9fbf66873a1abaf1d6a04bc
Diego Monroy <difemo91@gmail.com>
2019-11-27 01:48:08 +0000

    add number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all genres from 'hbtn_0d_tvshows and displays the number of shows linked to each'
+SELECT tg.name, COUNT(tsg.genre_id) AS number_of_shows
+FROM tv_genres AS tg
+INNER JOIN tv_show_genres AS tsg ON tsg.genre_id = tg.id
+GROUP BY tg.name
+ORDER BY number_of_shows DESC;
