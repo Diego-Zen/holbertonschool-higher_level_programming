01bad7447791ad32bf5fc736455dca17b1fa6b6e
Diego Monroy <difemo91@gmail.com>
2019-11-27 02:03:29 +0000

    fix order

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,5 @@ SELECT tg.name
 FROM tv_genres AS tg
 INNER JOIN tv_show_genres AS tsg ON tsg.genre_id = tg.id
 INNER JOIN tv_shows AS ts ON ts.id = tsg.show_id
-WHERE ts.title = 'Dexter';
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name ASC;
