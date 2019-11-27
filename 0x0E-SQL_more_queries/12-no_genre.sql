81c64eae66344e4ca4282d7e29bb09044fa9c08e
Diego Monroy <difemo91@gmail.com>
2019-11-27 01:29:43 +0000

    add no genre shows

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all shows without a genre linked
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+LEFT OUTER JOIN tv_show_genres AS tsg ON tsg.show_id = ts.id
+WHERE genre_id IS NULL
+ORDER BY ts.title, tsg.genre_id;
