9d535ee8dee5dbe12ba982044efa7e3b4e0d9d4e
Diego Monroy <difemo91@gmail.com>
2019-11-27 02:01:49 +0000

    add list all genres of the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- Lists all genres of the show 'Dexter'
+SELECT tg.name
+FROM tv_genres AS tg
+INNER JOIN tv_show_genres AS tsg ON tsg.genre_id = tg.id
+INNER JOIN tv_shows AS ts ON ts.id = tsg.show_id
+WHERE ts.title = 'Dexter';
