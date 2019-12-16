640de9d8753c31b017d48673ce220f8f22e7c8af
Diego Monroy <difemo91@gmail.com>
2019-12-16 05:36:01 +0000

    [FIX] add query option

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
+"""Lists all states where name matches the argument"""
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    """Lists all states where name matches the argument"""
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
     c = db.cursor()
-    c.execute("""
-    SELECT * FROM states
-    WHERE name = '{}'
-    ORDER BY states.id ASC
-    """.format(argv[4])
+    c.execute("""SELECT * FROM states
+    WHERE name = %s
+    ORDER BY states.id ASC""", (argv[4],))
     res = c.fetchall()
     for row in res:
             print(row)
