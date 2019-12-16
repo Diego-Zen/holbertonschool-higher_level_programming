65583a8624911b944f19e390a17d2c71a42b3293
Diego Monroy <difemo91@gmail.com>
2019-12-16 05:14:25 +0000

    [FEAT] add filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """Lists all states where name matches the argument"""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    arg = argv[4]
+    c.execute("SELECT * FROM states "
+              "WHERE states.name = %s "
+              "ORDER BY states.id ASC", (arg,))
+    res = c.fetchall()
+    for row in res:
+            print(row)
+    c.close()
+    db.close()
