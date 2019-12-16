baf14ccc60559771ff20e1a130690b07a21984b5
Diego Monroy <difemo91@gmail.com>
2019-12-16 04:04:46 +0000

    [FEAT] add script that lists all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """Lists all states"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY states.id ASC")
+    res = c.fetchall()
+    for row in res:
+            print(row)
+    c.close()
+    db.close()
