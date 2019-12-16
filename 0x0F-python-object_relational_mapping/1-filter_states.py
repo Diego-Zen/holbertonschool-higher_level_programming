81bda53caa05c88b3b143ead93e65da8da54bbe5
Diego Monroy <difemo91@gmail.com>
2019-12-16 04:20:48 +0000

    [FEAT] add filter states by name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """Lists all states with name starting with N"""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states "
+              "WHERE LEFT(name, 1) = 'N' "
+              "ORDER BY states.id ASC")
+    res = c.fetchall()
+    for row in res:
+            print(row)
+    c.close()
+    db.close()
