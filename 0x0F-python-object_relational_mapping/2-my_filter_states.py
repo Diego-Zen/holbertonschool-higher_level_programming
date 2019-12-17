f8ac6d8d9256423be7ea906d7c84c240943f1d95
Diego Monroy <difemo91@gmail.com>
2019-12-17 03:20:39 +0000

    [FIX] add format for query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,17 +4,17 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states
-    WHERE name = %s
-    ORDER BY states.id ASC""", (argv[4],))
-    res = c.fetchall()
-    for row in res:
-            print(row)
-    c.close()
-    db.close()
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states
+    WHERE name = "{}" ORDER BY states.id ASC""".format(argv[4]))
+    query_res = cur.fetchall()
+    for row in query_res:
+            print("{}".format(row))
+    cur.close()
+    conn.close()
