abf9de86428b4f70cd2a9e01ca38e7f0bde90108
Diego Monroy <difemo91@gmail.com>
2019-12-17 05:10:33 +0000

    [FEAT] add script safe of SQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Lists all states where name matches the argument"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states
+    WHERE name LIKE BINARY "{}" ORDER BY states.id ASC""".format(argv[4]))
+    query_res = cur.fetchall()
+    for row in query_res:
+            print("{}".format(row))
+    cur.close()
+    conn.close()
