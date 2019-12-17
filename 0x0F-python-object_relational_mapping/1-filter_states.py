f135293b4a1d7b6e9a875408c97e4aaab67060b6
Diego Monroy <difemo91@gmail.com>
2019-12-17 02:54:39 +0000

    [FIX] pep8 spaces complain

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,11 +5,11 @@ import MySQLdb
 if __name__ == "__main__":
     """Lists all states with name starting with N"""
     conn = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         charset="utf8")
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8")
     cur = conn.cursor()
     cur.execute("""SELECT * FROM states
     WHERE ASCII(name) = 78
