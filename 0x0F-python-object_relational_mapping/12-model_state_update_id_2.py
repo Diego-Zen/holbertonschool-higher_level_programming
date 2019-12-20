4a3966266dc4682380603dff69ec3a31b46f306e
Diego Monroy <difemo91@gmail.com>
2019-12-20 19:45:50 +0000

    [FEAT] add update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+    Update a state module
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import update
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    stmt = (
+        update(State)
+        .values(name='New Mexico')
+        .where(State.id == 2)
+    )
+    session.execute(stmt)
+    session.commit()
+
+    session.close()
