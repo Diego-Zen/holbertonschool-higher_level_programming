02a66f4e67a753d3e7b91fa70ad8e69b9e80f920
Diego Monroy <difemo91@gmail.com>
2019-12-20 19:53:40 +0000

    [FEAT] add delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+    Delete states that contain a letter module
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import delete
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    stmt = (
+        delete(State)
+        .where(State.name.contains('a'))
+    )
+    session.execute(stmt)
+    session.commit()
+
+    session.close()
