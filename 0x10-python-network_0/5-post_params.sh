5e287b16f3b8afc7fa7e1d23a370b32f4a983a88
Diego Monroy <difemo91@gmail.com>
2020-01-07 22:20:01 +0000

    [FEAT] add POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# This script sends a POST request and displays the body response
+curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
