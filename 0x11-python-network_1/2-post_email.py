14c6a0fffce95cb7b9e4925393c54008435b08e6
Diego Monroy <difemo91@gmail.com>
2020-01-13 23:31:24 +0000

    [FEAT] add post an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+    Takes a url and a an email and sends a post request module
+"""
+import sys
+from urllib import request
+from urllib import parse
+
+if __name__ == "__main__":
+    post_data = parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    with request.urlopen(url=sys.argv[1], data=post_data) as post_response:
+        data = post_response.read()
+        dec = data.decode('ascii')
+        print(dec)
