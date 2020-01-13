c6133632712abab5aa562bda21704c1e138b12ad
Diego Monroy <difemo91@gmail.com>
2020-01-13 20:25:32 +0000

    [FEAT] add whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+    fetches url
+"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        resp = response.read()
+        charset = resp.decode('utf-8')
+        print("Body response:")
+        print(
+            "\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+            .format(type(resp), resp, charset)
+        )
