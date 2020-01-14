241f48ee0beaab5a6068ff3fd6e6b75ebe22cff0
Diego Monroy <difemo91@gmail.com>
2020-01-14 02:24:11 +0000

    [FEAT] add error code script

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+    Error code module
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    status = r.status_code
+    if (status >= 400):
+        print("Error code: {}".format(status))
+    else:
+        print(r.text)
