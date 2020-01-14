c4e13fd72510d939378ababc59d10c1083de2f8a
Diego Monroy <difemo91@gmail.com>
2020-01-14 00:13:48 +0000

    [FEAT] add error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+    Takes in a url, sends a request to the url and displays the body module
+"""
+import sys
+from urllib import request
+from urllib import parse
+from urllib import error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(url=sys.argv[1]) as response:
+            data = response.read()
+            dec = data.decode('utf-8')
+            print(dec)
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
