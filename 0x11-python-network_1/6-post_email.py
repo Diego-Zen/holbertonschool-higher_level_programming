48914a58de7ccd3a17acf65647dcd5ee5161643b
Diego Monroy <difemo91@gmail.com>
2020-01-14 02:12:46 +0000

    [FEAT] add post an email with requests package

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+    Post an email module
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
