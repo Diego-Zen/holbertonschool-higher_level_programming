9bbf2849bb999a07832012146d5ed0882ebde069
Diego Monroy <difemo91@gmail.com>
2020-01-14 01:39:00 +0000

    [FEAT] add response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+    response header value module
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('x-request-id'))
