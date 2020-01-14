395519e61550d1e0f389e5a19c617f26b57d339d
Diego Monroy <difemo91@gmail.com>
2020-01-14 00:19:00 +0000

    [FEAT] add my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+    fetches url
+"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        resp = response.read()
+        cont = resp.decode('utf-8')
+        print("Body response:")
+        print(
+            "\t- type: {}\n\t- content: {}"
+            .format(type(cont), cont)
+        )
