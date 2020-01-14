b2d717dc9a3b02af28c8262b79a23834c88ff2e2
Diego Monroy <difemo91@gmail.com>
2020-01-14 04:49:37 +0000

    [FEAT] add github api script

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+    Takes github credentials and uses the Github API module
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    try:
+        print(r.json().get('id'))
+    except ValueError:
+        print("Not a valid JSON")
