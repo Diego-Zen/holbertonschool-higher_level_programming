ad1810e4d06520c7054f81b2b5a5da31f8285f1f
Diego Monroy <difemo91@gmail.com>
2020-01-14 04:35:14 +0000

    [FEAT] add Star Wars API

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+    Star Wars api search module
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    search = sys.argv[1]
+    r = requests.get('https://swapi.co/api/people/', params={'search': search})
+    try:
+        json_data = r.json()
+        print("Number of results: {}".format(json_data.get('count')))
+        for test in json_data['results']:
+            print(test.get('name'))
+    except ValueError:
+        print("Not a valid JSON")
