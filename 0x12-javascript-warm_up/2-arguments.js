e806231af24f22c0ac2d9f23b3e1181a12a83d1a
Diego Monroy <difemo91@gmail.com>
2020-02-04 16:57:14 +0000

    [FEAT] add script that depending on the arguments passed prints something

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+const myVar = process.argv;
+if (myVar.length > 2) {
+  if (myVar.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
+} else { console.log('No argument'); }
