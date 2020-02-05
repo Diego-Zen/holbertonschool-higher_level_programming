05128bb111de005f0da468f2a0f0815a6207b37e
Diego Monroy <difemo91@gmail.com>
2020-02-05 02:54:33 +0000

    [FEAT] add script that prints a square of size of the argument given

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const myArgs = process.argv;
+if (isNaN(parseInt(myArgs[2]))) {
+  console.log('Missing size');
+} else {
+  printSquare(parseInt(myArgs[2]));
+}
+
+function printSquare (size) {
+  let i, j;
+  for (i = 0; i < size; i++) {
+    let myStr = '';
+    for (j = 0; j < size; j++) {
+      myStr = myStr + 'X';
+    }
+    console.log(myStr);
+  }
+}
