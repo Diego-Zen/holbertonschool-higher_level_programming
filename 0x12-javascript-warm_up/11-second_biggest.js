0754de0c65c1d1cce99cb25086a96657cc04e4b9
Diego Monroy <difemo91@gmail.com>
2020-02-05 04:49:00 +0000

    [FEAT] add second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const myArgs = process.argv;
+if (myArgs.length < 4) {
+  console.log(0);
+} else {
+  let i;
+  const myNumbers = [];
+  let myOrdered = [];
+  for (i = 2; i < myArgs.length; i++) {
+    myNumbers.push(parseInt(myArgs[i]));
+  }
+  myOrdered = myNumbers.sort(function (a, b) { return a - b; });
+  console.log(myOrdered[myOrdered.length - 2]);
+}
