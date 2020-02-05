0caac05dfea001c9c1ba9faa4a22a03be9246af0
Diego Monroy <difemo91@gmail.com>
2020-02-05 02:32:31 +0000

    [FEAT] add script that prints a string x number of times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const myArgs = process.argv;
+let i;
+if (isNaN(myArgs[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < myArgs[2]; i++) {
+    console.log('C is fun');
+  }
+}
