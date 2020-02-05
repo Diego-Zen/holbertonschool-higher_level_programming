0f7872f71cd26f401223073f82583757975926a4
Diego Monroy <difemo91@gmail.com>
2020-02-05 04:31:56 +0000

    [FEAT] add script that computes factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const myArgs = process.argv;
+if (myArgs.length < 3) {
+  console.log(1);
+} else {
+  console.log(myFactorial(myArgs[2]));
+}
+
+function myFactorial (x) {
+  if (x > 0) {
+    return x * myFactorial(x - 1);
+  } else {
+    return 1;
+  }
+}
