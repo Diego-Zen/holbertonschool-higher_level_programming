c73c38d68d18f65f4d8958afcc6931b06f0deeb2
Diego Monroy <difemo91@gmail.com>
2020-02-05 03:20:02 +0000

    [FEAT] add a script that add 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const myArgs = process.argv;
+if (myArgs.length < 4) {
+  console.log(NaN);
+} else {
+  add(parseInt(myArgs[2]), parseInt(myArgs[3]));
+}
+
+function add (a, b) {
+  const res = a + b;
+  console.log(res);
+}
