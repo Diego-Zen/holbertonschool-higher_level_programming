62e2f2445d75ac579ffef37a25e5fe0822325e66
Diego Monroy <difemo91@gmail.com>
2020-02-11 05:29:02 +0000

    [FIX] add space after function declaration

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const map1 = list.map(function(num, index) {
+const map1 = list.map(function (num, index) {
   return num * index;
 });
 console.log(list);
