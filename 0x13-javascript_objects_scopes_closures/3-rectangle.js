f1f7d0f00104e8e2f4d368f7f790560db348a16c
Diego Monroy <difemo91@gmail.com>
2020-02-11 02:08:17 +0000

    [FEAT] add rectangle class with print method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i, j;
+    for (i = 0; i < this.height; i++) {
+      let myStr = '';
+      for (j = 0; j < this.width; j++) {
+        myStr = myStr + 'X';
+      }
+      console.log(myStr);
+    }
+  }
+}
+
+module.exports = Rectangle;
