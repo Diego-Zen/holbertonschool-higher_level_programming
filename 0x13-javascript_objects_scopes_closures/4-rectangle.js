05d8475ecf66e58f654d98c591dca58e7de570fc
Diego Monroy <difemo91@gmail.com>
2020-02-11 02:15:18 +0000

    [FEAT] add rotate and double methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,33 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i, j;
+    for (i = 0; i < this.height; i++) {
+      let myStr = '';
+      for (j = 0; j < this.width; j++) {
+        myStr = myStr + 'X';
+      }
+      console.log(myStr);
+    }
+  }
+
+  rotate () {
+    const i = this.height;
+    this.height = this.width;
+    this.width = i;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
