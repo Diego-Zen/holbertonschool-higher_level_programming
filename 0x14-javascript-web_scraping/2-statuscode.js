f5d3389c957e32f0b7b82e99d68eeb7744ecde93
Diego Monroy <difemo91@gmail.com>
2020-02-12 04:47:25 +0000

    [FEAT] add status code request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const myArgs = process.argv;
+const request = require('request');
+
+request(myArgs[2], function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
+});
