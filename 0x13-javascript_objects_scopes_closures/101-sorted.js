391750d27d1f97576fd45fa528dae6b677178c4d
Diego Monroy <difemo91@gmail.com>
2020-02-12 02:40:27 +0000

    [FEAT] add sorted occurences dictionary

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
-exports.dict = function () {}
+
+const dict = require('./101-data').dict;
+
+const result = Object.keys(dict);
+const myDict = {};
+
+for (let i = 0; i < result.length; i++) {
+  if (myDict[dict[result[i]]] === undefined) {
+    myDict[dict[result[i]]] = [result[i]];
+  } else {
+    myDict[dict[result[i]]].push(result[i]);
+  }
+}
+
+console.log(myDict);
