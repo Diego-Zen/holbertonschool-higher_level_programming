01c6fd0de6a65f956cca9fda47f1edb576f2d9d7
Diego Monroy <difemo91@gmail.com>
2020-02-12 04:33:43 +0000

    [FEAT] add read a file and print its contents

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const myArgs = process.argv;
+const fs = require('fs');
+
+fs.readFile(myArgs[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
