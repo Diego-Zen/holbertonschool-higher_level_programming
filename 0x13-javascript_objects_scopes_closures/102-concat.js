4fc0c0a4475df893260cc6355d4da4478213288f
Diego Monroy <difemo91@gmail.com>
2020-02-12 04:18:14 +0000

    [FEAT] add concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
+
 const myArgs = process.argv;
-if (myArgs[2] === undefined) {
-  console.log('missing');
-} else {
-  console.log('C is fun!');
-}
+const fs = require('fs');
+
+const fileA = fs.readFileSync(myArgs[2], 'utf-8', (err, data) => {
+  if (err) throw err;
+});
+
+const fileB = fs.readFileSync(myArgs[3], 'utf-8', (err, data) => {
+  if (err) throw err;
+});
+
+const result = fileA + '\n' + fileB + '\n';
+
+fs.writeFileSync(myArgs[4], result, 'utf-8', (err, data) => {
+  if (err) throw err;
+});
