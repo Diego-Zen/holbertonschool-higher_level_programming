8f6693d35dd1b5ddda4177ea6cd7990fec3b71f9
Diego Monroy <difemo91@gmail.com>
2020-02-13 13:59:08 +0000

    [FEAT] add toggle class

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+$('DIV#toggle_header').click(function () {
+  if ($('HEADER').hasClass('red')) {
+    $('HEADER').addClass('green').removeClass('red').css('color', '#FF0000');
+  } else {
+    $('HEADER').addClass('red').removeClass('green');
+  }
+});
