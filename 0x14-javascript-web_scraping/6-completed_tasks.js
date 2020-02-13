b6f75c5b26d2bc83016cbb9f8177d93df60bfc25
Diego Monroy <difemo91@gmail.com>
2020-02-13 10:31:51 +0000

    [FEAT] add script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const myArgs = process.argv;
+const request = require('request');
+const url = myArgs[2];
+const myDict = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const jsonData = JSON.parse(body);
+    jsonData.forEach((id) => {
+      if (id.completed === true && myDict[id.userId] === undefined) {
+        myDict[id.userId] = 1;
+      } else if (id.completed === true) {
+        myDict[id.userId] = myDict[id.userId] + 1;
+      }
+    });
+  }
+
+  console.log(myDict);
+});
