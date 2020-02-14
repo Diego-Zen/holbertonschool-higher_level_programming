f289ecb9fe546db288b1d34c0cd73ff3b13ab499
Diego Monroy <difemo91@gmail.com>
2020-02-14 14:37:31 -0500

    [FEAT] add characters ordered

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,12 +1,26 @@
 #!/usr/bin/node
 
 const myArgs = process.argv;
-const fs = require('fs');
+const request = require('request');
+const movieID = myArgs[2];
+const url = 'https://swapi.co/api/films/' + movieID;
+let urlChar;
 
-fs.readFile(myArgs[2], 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data);
+    const jsonData = JSON.parse(body).characters;
+    const len = jsonData.length;
+    for (let i = 0; i < len; i++) {
+      urlChar = jsonData[i];
+      request(urlChar, function (err, response, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(data).name);
+        }
+      });
+    }
   }
 });
